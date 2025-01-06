f7c3998c85e8e350a46c705e91925c07b79547fa
cgdisgood <cgamerdoggo@gmail.com>
2025-01-05 20:02:41 -0600

    add music player

--- api/index.py
+++ api/index.py
@@ -1,7 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import datetime
+import json
+from urllib.parse import unquote
+
+with open("api/songs.json", 'r', encoding='utf-8') as playlist:
+    songs = json.load(playlist)
+
+print(songs)
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    date = int(datetime.datetime.now().strftime("%d")) - 1
+
+    sootd = songs['songs'][date]
+    return render_template('index.html', sootd = sootd)
+
+@app.route('/songs/<path:filename>')
+def serve_audio(filename):
+    return send_from_directory('songs', unquote(filename))
+
+@app.route('/songs.json')
+def songsList():
+    return send_from_directory('', 'songs.json')
+
+app.run(port=3000)
